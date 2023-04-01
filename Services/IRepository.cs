7c4e6b6d50d5117739563ac1a638bbdf9cf91c31
James Wickman <nauticalnova@hotmail.com>
2023-04-01 15:52:47 -0600

    first set

---
+++ Services/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using Inventory_tracking_basic.Models;
+
+namespace Inventory_tracking_basic.Services
+{
+    public interface IRepository
+    {
+        public User Auth(LoginModel login);
+        public void Register(LoginModel login);
+        public User Get(string email);
+    }
+}
