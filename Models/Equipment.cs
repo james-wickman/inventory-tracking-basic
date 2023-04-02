5e56058ccc3cd1c471e62c91944c219290627b51
James Wickman <nauticalnova@hotmail.com>
2023-04-02 09:55:00 -0600

    setting basic models

---
+++ Models/Equipment.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inventory_tracking_basic.Models
+{
+    public class Equipment
+    {
+        public int UserId { get; set; }
+        public string EquipmentName { get; set; }
+        public string Amount { get; set; }
+        public string EquipmentId { get; set; }
+
+
+
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -13,5 +13,8 @@ namespace Inventory_tracking_basic.Models
         public string UserName { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+
     }
 }
